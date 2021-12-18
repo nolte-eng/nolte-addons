93a49d651d1547f4094fc0e0fa414eea2a15d2d5
root <root@localhost.localdomain>
2021-12-18 21:31:20 +0000

    Fehlerbereinigung

--- nolte_berichte/models/res_company.py
+++ nolte_berichte/models/res_company.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# Copyright 2021-Nolte engineering GmbH
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
