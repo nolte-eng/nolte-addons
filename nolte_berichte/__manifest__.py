1e154ed26fca0ad900746bca4e5f5b3b89eab829
Lars Nolte <nolte-eng@users.noreply.github.com>
2023-11-28 13:40:19 +0100

    Update __manifest__.py

--- nolte_berichte/__manifest__.py
+++ nolte_berichte/__manifest__.py
@@ -11,27 +11,11 @@
     'depends':['base', 'account', 'web', 'sale'],
 
     'data':[
-
-       # 'report/external_layout_german.xml',
-        'report/xpath_invoice_document.xml',
-        'report/xpath_din5008.xml',
-      #  'report/report_invoice_document.xml',
-       # 'report/german_din5008.xml',
         'report/german_invoice_document.xml',
         'views/report_templates.xml',
-       # 'report/report_invoice_document.xml',
-        'report/external_layout_german.xml',
-        'report/report_invoice_document.xml',
-        # 'report/report_saleorder_document.xml',
-        # 'report/report_purchaseorder_document.xml',
-        #
-        # 'views/address_layout.xml',
-        # 'views/company_footer.xml',
-           'views/res_company_view.xml',
-        #
-        # 'data/paper_format.xml',
+        'views/res_company_view.xml',
+       
         'data/report_layout.xml'
-
     ],
     'assets': {
         'web.report_assets_common': [
