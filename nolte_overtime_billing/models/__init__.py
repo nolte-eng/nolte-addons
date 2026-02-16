44f4d3860b56417940122021e75163d029b5563e
Lars Nolte <nolte-eng@users.noreply.github.com>
2026-02-16 14:02:14 +0100

    Delete nolte_overtime_billing directory

--- nolte_overtime_billing/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import product_template
-from . import res_config_settings
-
-from . import nolte_overtime_settings
-
--- nolte_overtime_billing/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import overtime_import_wizard
